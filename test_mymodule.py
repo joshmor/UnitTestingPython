975dcfaedd39388bc8461404b9c6e9251e963db4
root <root@PE-Safe-Ari.localdomain>
2022-03-03 15:45:15 +0000

    Updated test files

--- test_mymodule.py
+++ test_mymodule.py
@@ -2,13 +2,14 @@
 import unittest
 from mymodule import square, double
 
-class TestMyModule(unittest.TestCase):
-    #create 2 test functions they must always start with test
+#create 2 test functions they must always start with test
+class TestSquare(unittest.TestCase): 
     def test_square(self):
         self.assertEqual(square(2),4)
+
+class TestDouble(unittest.TestCase):
     def test_double(self):
         self.assertEqual(double(4),8)
 
-if __name__ == '__main__':
-    unittest.main()
+unittest.main()
     
